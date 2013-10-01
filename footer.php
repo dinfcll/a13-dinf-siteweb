ed85493719718b5bb2eb454c94dfea661ce1a572
etanguay <etienne.tanguay@cll.qc.ca>
2013-10-01 08:33:48 -0400

    footer + css

--- footer.php
+++ footer.php
@@ -1,19 +1,51 @@
 <div id="templatemo_footer">
 
     <ul class="footer_menu">
-        <li><a href="#">Home</a></li>
-        <li><a href="http://www.templatemo.com" target="_parent">CSS Templates</a></li>
-        <li><a href="http://www.flashmo.com" target="_parent">Flash Files</a></li>
-        <li><a href="#">Gallery</a></li>
-        <li><a href="#">About</a></li>
+        <li><a href="index.html">Accueil</a></li>
+        <li><a href="http://cll.qc.ca">Cégep Lévis-Lauzon</a></li>
+        <li><a href="https://cll.omnivox.ca" target="_parent">Omnivox</a></li>
+        <li><a href="http://cll.moodle.decclic.qc.ca/">Moodle</a></li>
+        <li><a href="http://e5.onthehub.com/WebStore/ProductsByMajorVersionList.aspx?ws=6c5a70be-a08b-e011-969d-0030487d8897&vsro=8&JSEnabled=1">MSDNAA</a></li>
         <li class="last_menu"><a href="#">Contact</a></li>
     </ul>
 
-    Copyright © 2013 <a href="#">Cegep-Levis-Lauzon</a> |
-    Fait par Etienne Tanguay, Etienne Lafond, Alexandre Gosselin |
+    <TABLE>
+        <TR>
+            <TD WIDTH="40%" HEIGHT="33%">
+                <h3>À propos</h3>
+                </br>
+                <p>
+                    Le Cégep de Lévis-Lauzon, établissement public d'enseignement supérieur, a pour mission de contribuer
+                    au développement de sa région et du Québec par une formation qualifiante, ouverte sur le monde et par
+                    des services aux entreprises et aux organisations répondant à leurs besoins.
+                </p>
+            </TD>
+            </TD>
+            <TD WIDTH="12%" HEIGHT="33%"></TD>
+            <TD WIDTH="18%" HEIGHT="33%"><a target="_blank" href="http://maps.google.ca/maps?q=C%C3%A9gep+L%C3%A9vis-Lauzon,+L%C3%A9vis,+QC&hl=fr&ie=UTF8&sll=49.891235,-97.15369&sspn=50.70371,129.550781&oq=C%C3%A9gep+L%C3%A9vi&t=h&hq=C%C3%A9gep+L%C3%A9vis-Lauzon,+L%C3%A9vis,+QC&z=15&iwloc=A">
+                    <img alt="Carte" src="http://cll.qc.ca/admin/wp-content/themes/clevislauzon/img/img_pied_carte.jpg" WIDTH="95%" HEIGHT="65%"/>
+                </a></TD>
+            <TD WIDTH="30%" HEIGHT="33%">
+                <h4>Adresse</h4>
+                <p>205,&nbsp;route&nbsp;Mgr&nbsp;Bourget,</br>Lévis,&nbsp;(Québec), G6V&nbsp;6Z9</p>
+                <h4>COORDONATEUR </br>Département d'informatique</h4>
+                <p>Stéphane Mercier<BR></p>
+                <p><a HREF="mailto:stephane.mercier@cll.qc.ca">stephane.mercier@cll.qc.ca</a></p>
+                <h4>Téléphone</h4>
+                <p>418 833-5110 poste 3826</p>
+            </TD>
+        </TR>
+    </TABLE>
 
+
+    Conçu par : Étienne Lafond, Etienne Tanguay et Alexandre Gosselin</br>
+    Finissants en Techique Informatique - Gestion de Réseaux - Cégep Lévis-Lauzon - Automne 2013</br>
+    Copyright © 2048  <a href="http://www.templatemo.com" target="_parent">Free CSS Templates</a> |
+    Validate <a href="http://validator.w3.org/check?uri=referer">XHTML</a> &amp; <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>
 
 </div>
 
 <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js'></script>
 <script type='text/javascript' src='js/logging.js'></script>
+</body>
+</html>
