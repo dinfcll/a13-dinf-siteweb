ee974808b0e832272b849f2a9adc4e593c2dfb73
etanguay <etienne.tanguay@cll.qc.ca>
2013-11-12 11:18:48 -0500

    menu rapide #4

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
        <!-- <p class="bar_nav5">l'informatique</p> -->
         <div id="menurapide">
             <ul>
-                <li><a href="#">Menu</a>
+                <li><a href="#">Menu Rapide</a>
                     <ul>
                         <li><a href="https://cll.omnivox.ca/intr/Module/Identification/Login/Login.aspx?ReturnUrl=/intr" target="_new">Omnivox</a></li>
                         <li><a href="https://alphonse2.clevislauzon.qc.ca/gw/webacc" target="_new">Messagerie</a></li>
