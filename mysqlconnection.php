b8fef121d5b098ed2d820e9cdc507f683f45c0c8
etanguay <etienne.tanguay@cll.qc.ca>
2013-11-26 11:27:33 -0500

    Bonnes informations de connexion

--- mysqlconnection.php
+++ mysqlconnection.php
@@ -1,9 +1,9 @@
 <?php
-$link = mysql_connect("localhost", "etienne", "etienne")
+$link = mysql_connect("localhost", "equipe5", "equipe5abc")
 or die("Impossible de se connecter : " . mysql_error());
 //echo 'Connexion réussie';
 
-$db_selected = mysql_select_db('SiteWebDepInfo', $link);
+$db_selected = mysql_select_db('a13equipe5', $link);
 if (!$db_selected) {
     die ('Impossible de sélectionner la base de données : ' . mysql_error());
 }
